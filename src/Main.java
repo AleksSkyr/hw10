633f1585ec2fc688a7f56b5a00e995cc42fa4e3e
AleksSkyr <greshnik1499@gmail.com>
2023-05-23 17:48:19 +0300

    В чём ошибка? не выводит значение 2 и 3 задачи?

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,49 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        tasc1();
+        leapYear(200);
+        printDevice(2019);
+        tasc3(30);
+
+    }
+
+    public static void leapYear(int year) {
+        System.out.println("Задача 1");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " Год является высокосным!");
+        } else {
+            System.out.println("Этот год не являеться высокосным");
+        }
+    }
+
+    public static void printDevice(int clientOs) {
+        System.out.println("Задача 2");
+        int versionOs = LocalDate.now().getYear();
+        if (clientOs == 1 && versionOs <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOs == 1 && versionOs >= 2015) {
+            System.out.println("Установите программу для Android по ссылке");
+        }
+        if (clientOs == 0 && versionOs >= 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (clientOs == 0 && versionOs <= 2105)
+            System.out.println("Установите программу для IOS по ссылке");
+
+    }
+
+    private static int tasc3(int distance) {
+        System.out.println("Задача 3");
+        if (distance > 100) {
+            System.out.println("Доставка невозможна");
+            return -1;
+        }
+        int deliveryDey = 1;
+        if (distance > 20) {
+        } else if (distance > 60) {
+            deliveryDey++;
+            System.out.println("Доставка составит " + deliveryDey + " дней");
+        }
+        return deliveryDey;
     }
 }
